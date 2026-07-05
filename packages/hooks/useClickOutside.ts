bc46122b8fe63fe6aaaf49d25373fd61e714eb72
c-shibw01 <13972186114@139.com>
2026-07-05 12:44:23 +0800

    useEventListener 事件不触发的bug

--- packages/hooks/useClickOutside.ts
+++ packages/hooks/useClickOutside.ts
@@ -1,11 +1,11 @@
-import { type Ref, onMounted } from "vue";
+import { type Ref } from "vue";
 import useEventListener from "./useEventListener";
 
 export default function useClickOutside(
   elementRef: Ref<HTMLElement | void>,
   callback: (e: MouseEvent) => void,
 ) {
-  onMounted(() => {
+  if (typeof document !== "undefined") {
     useEventListener(document, "click", (e: Event) => {
       if (elementRef.value && e.target) {
         if (!elementRef.value.contains(e.target as HTMLElement)) {
@@ -13,5 +13,5 @@ export default function useClickOutside(
         }
       }
     });
-  });
+  }
 }
